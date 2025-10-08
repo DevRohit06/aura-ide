bbd05688ae260d25ba3418eda367783cc3668140
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-10-08 20:04:13 +0530

    fix: reorder import statements in login form component

--- src/lib/components/login-form.svelte
+++ src/lib/components/login-form.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
