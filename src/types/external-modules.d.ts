a2304c910838df7c34a1ded59caf95302de1ade9
Rohit Kushwaha <technicalrohit06@gmail.com>
2025-12-31 01:00:53 +0530

    refactor: Remove E2B code interpreter module declaration

--- src/types/external-modules.d.ts
+++ src/types/external-modules.d.ts
@@ -1,5 +1,4 @@
 declare module '@daytona/sdk';
-declare module '@e2b/code-interpreter';
 declare module '@qdrant/js-client-rest';
 declare module '@modelcontextprotocol/*';
 declare module '@daytona/*';
